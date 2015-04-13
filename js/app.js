72e44a5970aeeb82083d75cfa599ef5b8b6c444f
Anny Gakhokidze <annygakhokidze@gmail.com>
2015-04-13 01:37:33 -0700

    basic heatmap is done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 $(function (){
 
 
-	var map;
+	var map, map_div;
 	var ubc_lat = 49.2611,
 		ubc_long = -123.2531;
+	var heatmap;
+
 	var ubc_latlong;
 	var points = [];
+	var markers = [];
 
+
+	var bounding_box;
 
 	// Foursquare Options URL
 	var time_options = '&time=any';
@@ -15,25 +20,30 @@ $(function (){
 	var CLIENT_SECRET = 'OHO4BCL1FHIGBA0PXGFEJDRQBAEPPBX3L2WJ43HNBXCGH4RZ';
 	var type = '&section=food';
 	var VERSION = 20130815;
-	var api = 'explore?'
+	var api = 'explore?';
+	var radius = '&radus=' + 10000;
+	var limit = '&limit=' + 50;
 	var URL = 'https://api.foursquare.com/v2/venues/' + api 
 				+ '&ll=' + ubc_lat + ',' + ubc_long
 				+ time_options 
 				+ day_options 
 				+ type
+				+ limit
 				+ '&client_id=' + CLIENT_ID 
 				+ '&client_secret=' + CLIENT_SECRET
 				+ '&v=' + VERSION;
 
 
-
+	var ZOOM_LEVEL = 13;
 
 	function initialize() {
 	  var mapOptions = {
-	    zoom: 6
+	    zoom: ZOOM_LEVEL
 	  };
-	  map = new google.maps.Map(document.getElementById('map-canvas'),
+	  map_div = document.getElementById('map-canvas');
+	  map = new google.maps.Map(map_div,
 	      						mapOptions);
+	  
 	  ubc_latlong = new google.maps.LatLng(ubc_lat, ubc_long);
 
 	  // Try HTML5 geolocation
@@ -45,15 +55,12 @@ $(function (){
 	    // Browser doesn't support Geolocation
 	    handle_no_geolocation(false);
 	  }
+	  // add google event listeners
+	  google.maps.event.addListener(map, 'dragend', on_center_changed);
 	}
 
 
-	$.ajax({
-		type: 'GET',
-		url: URL,
-		success: on_success_foursquare_request,
-		error: on_error_foursquare_request
-	});
+	send_request(URL);
 
 	// ---------------- Setting up listeners ---------------- 
 	$('#center-ubc').click(handle_center_on_ubc);
@@ -94,34 +101,114 @@ $(function (){
 	function on_success_foursquare_request(object){
 		console.log(object.response);
 		parse_response(object);
+		console.log(points[0]);
 		var points_google_array = new google.maps.MVCArray(points);
 		heatmap = new google.maps.visualization.HeatmapLayer({
-		    data: points_google_array
+		    data: points_google_array,
+		    radius: 30
 	    });
+	    // heatmap.setMap(null);
 	    heatmap.setMap(map);
+	}
+	function make_url(center_of_map){
+		console.log('remake_url');
+		var new_lat = center_of_map.k;
+		var new_lng = center_of_map.D;
+
+		var new_url = 'https://api.foursquare.com/v2/venues/' + api 
+					+ '&ll=' + new_lat + ',' + new_lng
+					+ time_options 
+					+ day_options 
+					+ type
+					+ radius
+					+ limit
+					+ '&client_id=' + CLIENT_ID 
+					+ '&client_secret=' + CLIENT_SECRET
+					+ '&v=' + VERSION;
+		return new_url;
+
+	}
+
+	function on_center_changed(){
+		var center_latlng = get_center_of_map();
+		var new_url = make_url(center_latlng);
+		var resp_obj = send_request(new_url);
+		heatmap.setMap(null);
+		delete_markers();
+	}
+	function get_center_of_map(){
+		var curr_map_center = map.getCenter();
+		console.log(curr_map_center);
+		return curr_map_center;
+	}
+	function send_request(new_url){
+		$.ajax({
+			type: 'GET',
+			url: new_url,
+			success: on_success_foursquare_request,
+			error: on_error_foursquare_request
+		});
 	}
 	function on_error_foursquare_request(err) {
 		console.log("Error retrieving places from foursquare");
 	}
 	function parse_response(object){
+		console.log('parse_response');
 		var resp = object.response;
-		console.log(resp.groups);
 		var group = resp.groups[0];
 		var items = group.items;
+		points = [];
 		for (var i = 0; i < items.length; i++){
 			var item = items[i];
 			var venue = item.venue;
-			// var name = 
+			var rating = venue.rating;
 
 			var location = venue.location;
 			var lat = location.lat;
 			var lng = location.lng;
 			var google_latlng = new google.maps.LatLng(lat, lng);
-			points.push(google_latlng);
-			var rating = venue.rating;
-			console.log(rating);
+			var weight;
+			weight = rating * 10;
+			var obj_to_save = {
+				location: google_latlng,
+				weight: weight
+			}
+			save_to_points_array(obj_to_save);
+			add_marker(google_latlng);
 		}
 	}
+	function save_to_points_array(obj_to_save){
+			points.push(obj_to_save);
+	}
+	function add_marker(google_latlng){
+		var marker = new google.maps.Marker({
+			position: google_latlng,
+			map: map,
+			opacity: 0.2
+		})
+		markers.push(marker);
+	}
+
+	// Sets the map on all markers in the array.
+	function set_all_map(map) {
+	  for (var i = 0; i < markers.length; i++) {
+	    markers[i].setMap(map);
+	  }
+	}
+
+	// Removes the markers from the map, but keeps them in the array.
+	function clear_markers() {
+	  set_all_map(null);
+	}
+
+	// Deletes all markers in the array by removing references to them.
+	function delete_markers() {
+	  clear_markers();
+	  markers = [];
+	}
+
+
 	// ---------------- Google maps stuff ----------------
 	google.maps.event.addDomListener(window, 'load', initialize);
+	
 });
